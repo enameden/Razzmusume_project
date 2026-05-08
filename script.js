458a002dc8619110f801d3bac96d688daf89dc34
enameden <mgpr7363@gmail.com>
2026-05-08 22:37:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -312,12 +312,6 @@ updateProbUI(probs);
     const rank=Math.floor(Math.random()*(max-min+1))+min;
 
     showResult(rank);
-    function showResult(rank){
-
-  console.log(document.getElementById("result"));
-
-  const result = document.getElementById("result");
-  }
 
   phaseIndex++;
 
